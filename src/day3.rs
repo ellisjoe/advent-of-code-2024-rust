d2e01ff94a62bebfac22969ad00fba5d76b63a91
Joe Ellis <ellis125@gmail.com>
2024-12-03 18:53:33 -0700

    Day 3 Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -20,4 +20,28 @@ mod tests {
             .sum();
         println!("result: {:?}", result);
     }
+
+    #[test]
+    fn part2() {
+        let string = fs::read_to_string(Path::new("./src/input/day-03.in")).unwrap();
+        let regex = Regex::new("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|do\\(\\)|don't\\(\\)").unwrap();
+
+        let mut enabled = true;
+        let mut total = 0;
+
+        for cap in regex.captures_iter(string.as_str()) {
+            match cap.get(0).unwrap().as_str() {
+                "do()" => enabled = true,
+                "don't()" => enabled = false,
+                _ => {
+                    if enabled {
+                        let x: i64 = cap[1].parse().unwrap();
+                        let y: i64 = cap[2].parse().unwrap();
+                        total += x * y;
+                    }
+                }
+            }
+        }
+        println!("total: {:?}", total);
+    }
 }
